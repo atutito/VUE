3111476cedbb76a9f897c68f464118626302c1a4
atutito <augustoalerino@hotmail.com>
2023-02-24 23:51:53 -0300

    primer commit js

---
+++ assets/js/past.js
@@ -0,0 +1,28 @@
+const fechaActual = Date.parse(eventsData.currentDate);
+
+console.log(typeof(fechaActual));
+
+let fichas = [];
+
+let newFichas = new DocumentFragment();
+let cuerpo = document.getElementById('cuerpo');
+
+for(let cartas of eventsData.events) {
+    if(Date.parse(cartas.date) < fechaActual){
+    fichas.push(cartas);
+    };
+};
+
+for (let elemento of fichas){
+    cuerpo.innerHTML += `<div class="col">
+                            <div class="card">
+                                <img src="${elemento.image}" class="card-img-top" alt="${elemento.name}" style= "width: 100%; height: 10vw; object-fit: cover;">
+                            <div class="card-body">
+                                <h5 class="card-title">${elemento.name}</h5>
+                            <p class="card-text">${elemento.description}</p>
+                            <p class="card-text">Price: $${elemento.price}</p>
+                                <a href="./card.html"><button type="button" class="btn btn-danger" style="float: right;">Info</button></a>
+                            </div>
+                            </div>
+                        </div>`
+};
