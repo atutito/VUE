f4a31aecf7883a90d6c4256ddfffdd554218e4a3
Augusto <augustoalerino@hotmail.com>
2023-03-08 20:51:56 -0300

    entrega final task3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,114 +1,100 @@
-
 // SE LLAMAN LAS CARDS DINÁMICAMENTE
 const fechaActual = Date.parse(eventsData.currentDate);
+let fichas = eventsData.events;
 
-let fichas = [];
+function tarjetas(array, contenedor) {
+    let cuerpo = document.getElementById(contenedor);
+    cuerpo.innerHTML = ''  
+    if (array.length == 0) {
+        cuerpo.innerHTML =  '<div class="w-100"><h5> No results to show, please refine your search and try again </h5></div>' 
 
-let cuerpo = document.getElementById('cuerpo');
+    }  else {
+        let fragmento = new DocumentFragment();
+    for (let elemento of array){
+        let div = document.createElement('div');
+        div.innerHTML += `<div class="col">
+                                <div class="card">
+                                    <img src="${elemento.image}" class="card-img-top" alt="${elemento.name}" style= "width: 100%; height: 20vw; object-fit: cover;">
+                                <div class="card-body">
+                                    <h5 class="card-title">${elemento.name}</h5>
+                                <p class="card-text">${elemento.description}</p>
+                                <p class="card-text">Price: $${elemento.price}</p>
+                                    <a href="./card.html?_id=${elemento._id}"><button type="button" class="btn btn-danger" style="float: right;">Details</button></a>
+                                </div>
+                                </div>
+                            </div>`
+        fragmento.appendChild(div);
+        };
+    cuerpo.appendChild(fragmento);
+    }
+}
 
-for(let cartas of eventsData.events) {
-    fichas.push(cartas);
-};
-
-console.log(fichas);
-
-let fragmento = new DocumentFragment();
-
-for (let elemento of fichas){
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="col">
-                            <div class="card">
-                                <img src="${elemento.image}" class="card-img-top" alt="${elemento.name}" style= "width: 100%; height: 20vw; object-fit: cover;">
-                            <div class="card-body">
-                                <h5 class="card-title">${elemento.name}</h5>
-                            <p class="card-text">${elemento.description}</p>
-                            <p class="card-text">Price: $${elemento.price}</p>
-                                <a href="./card.html?_id=${elemento._id}"><button type="button" class="btn btn-danger" style="float: right;">Details</button></a>
-                            </div>
-                            </div>
-                        </div>`
-    fragmento.appendChild(div);
-};
-
-cuerpo.appendChild(fragmento);
-
-
+tarjetas(fichas, 'cuerpo');
 
 
 // SE CREAN LAS ETIQUETAS DE LOS INPUTS DINÁMICAS
 let busqueda = document.getElementById('search');
 
-let categorias =[];
-
-for (categoria of eventsData.events){
-    categorias.push(categoria.category);
+function crearChecks(array){
+    let categorias =[];
+    for (categoria of array){
+        categorias.push(categoria.category);
+    }
+    const dupCats = categorias.filter((cat, indice) => {
+        return categorias.indexOf(cat) !== indice;
+    });
+    let fragmento2 = new DocumentFragment();
+    for (let elemento of dupCats){
+        let div = document.createElement('div');
+        div.innerHTML += `<div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" value="option">
+                            <label class="form-check-label" for="checkbox">${elemento} </label>
+                        </div>`
+                        
+        fragmento2.appendChild(div);
+    };
+    busqueda.appendChild(fragmento2);
 }
-
-const dupCats = categorias.filter((cat, indice) => {
-    return categorias.indexOf(cat) !== indice;
-});
-
-let fragmento2 = new DocumentFragment();
-
-for (let elemento of dupCats){
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" value="option">
-                        <label class="form-check-label" for="checkbox">${elemento} </label>
-                    </div>`
-                    
-    fragmento2.appendChild(div);
-};
-
-busqueda.appendChild(fragmento2);
+crearChecks(fichas);
 
 
 // SE ESCUCHAN LAS CHECKBOXES, SE GENERA ARRAY DE LABELS
-let hijos = busqueda.children;
-let arrayHijos = [];
-
-let checks = document.getElementsByClassName('form-check-input');
-
-arrayHijos.push(checks);
-
+let arrayTrimeado = [];
 let checkboxes = document.querySelectorAll('input[type=checkbox]')
-let arrayFiltrado = [];
-let chequeados = [];
-let categoriasChequeadas = [];
-let fichasChequeadas = [];
+checkboxes.forEach( checkbox  => checkbox.addEventListener('change',() => { 
+    let chequeados = [...checkboxes].filter(checkbox => checkbox.checked).map(elemento => elemento.nextElementSibling.innerHTML)
+     
+    arrayTrimeado = chequeados.map(element => {
+        return element.trim()});
+    console.log(arrayTrimeado);
+    filtroCruzado(fichas);
+}))
 
-checkboxes.forEach( checkbox  => { 
-     checkbox.addEventListener('change', verificarSeleccion) 
-     })
-function verificarSeleccion(){
-     chequeados = Array.from(checkboxes).filter(checkbox => checkbox.checked);
-     for (let i of chequeados){
-     arrayFiltrado.push(i.nextElementSibling.innerHTML);
-    }
-    categoriasChequeadas = [...new Set(arrayFiltrado)];
-    console.log(categoriasChequeadas);
-    for (let j of categoriasChequeadas){
-        for (let i=0; i<fichas.length; i++){
-        if (j == i.category) {
-            fichasChequeadas.push(i); 
-            console.log(fichasChequeadas);
-        } 
-        }    
-    }
-};
-
+function filtrarCategorias(arrayCategorias, arrayObjetos) {
+    return arrayCategorias.length === 0 ? arrayObjetos : arrayObjetos.filter(elemento => arrayCategorias.includes(elemento.category));
+}
 
 // FILTRO SEARCH
+let inputValue = ''
 let filtroBusqueda = document.getElementById('filtroBusqueda');
 let arrayFiltrado2 = [];
 
 const input = filtroBusqueda.addEventListener('keyup', (e) => {
-    let busqueda = e.target.value.toLowerCase();
-    console.log(busqueda);
-        for (let j; j < fichas.length; j++) {
-        if(j.name.includes(busqueda)) {
-            arrayFiltrado2.push(j);
-        };
-        console.log(arrayFiltrado2);
-        };
+    inputValue = e.target.value.toLowerCase();
+        filtroCruzado(fichas);
 });
+
+function busquedaPorInput(valor, arrayObjetos) {
+    if (valor == '') return arrayObjetos;
+    let arrayNuevo = arrayObjetos.filter(el => el.name.toLowerCase().includes(valor.toLowerCase().trim()))
+    console.log(arrayNuevo);
+    return arrayNuevo;
+}
+
+// ❌❌❌ FILTRO CRUZADO ❌❌❌
+
+function filtroCruzado(array){
+    let tarjetasFiltradas = filtrarCategorias(arrayTrimeado, array);
+    let checkFiltrados = busquedaPorInput(inputValue, tarjetasFiltradas);
+    tarjetas(checkFiltrados, 'cuerpo');
+}
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -3,31 +3,106 @@ const fechaActual = Date.parse(eventsData.currentDate);
 
 let fichas = [];
 
-let newFichas = new DocumentFragment();
-let cuerpo = document.getElementById('cuerpo');
-
 for(let cartas of eventsData.events) {
     if(Date.parse(cartas.date) > fechaActual){
     fichas.push(cartas);
     };
 };
 
-let fragmento = new DocumentFragment();
+// SE LLAMAN LAS CARDS DINÁMICAMENTE
+function tarjetas(array, contenedor) {
+    let cuerpo = document.getElementById(contenedor);
+    cuerpo.innerHTML = ''  
+    if (array.length == 0) {
+        cuerpo.innerHTML =  '<div class="w-100"><h5> No results to show, please refine your search and try again </h5></div>' 
 
-for (let elemento of fichas){
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="col">
-                            <div class="card">
-                                <img src="${elemento.image}" class="card-img-top" alt="${elemento.name}" style= "width: 100%; height: 20vw; object-fit: cover;">
-                            <div class="card-body">
-                                <h5 class="card-title">${elemento.name}</h5>
-                            <p class="card-text">${elemento.description}</p>
-                            <p class="card-text">Price: $${elemento.price}</p>
-                                <a href="./card.html"><button type="button" class="btn btn-danger" style="float: right;">Info</button></a>
-                            </div>
-                            </div>
+    }  else {
+        let fragmento = new DocumentFragment();
+    for (let elemento of array){
+        let div = document.createElement('div');
+        div.innerHTML += `<div class="col">
+                                <div class="card">
+                                    <img src="${elemento.image}" class="card-img-top" alt="${elemento.name}" style= "width: 100%; height: 20vw; object-fit: cover;">
+                                <div class="card-body">
+                                    <h5 class="card-title">${elemento.name}</h5>
+                                <p class="card-text">${elemento.description}</p>
+                                <p class="card-text">Price: $${elemento.price}</p>
+                                    <a href="./card.html?_id=${elemento._id}"><button type="button" class="btn btn-danger" style="float: right;">Details</button></a>
+                                </div>
+                                </div>
+                            </div>`
+        fragmento.appendChild(div);
+        };
+    cuerpo.appendChild(fragmento);
+    }
+}
+
+tarjetas(fichas, 'cuerpo');
+
+
+// SE CREAN LAS ETIQUETAS DE LOS INPUTS DINÁMICAS
+let busqueda = document.getElementById('search');
+
+function crearChecks(array){
+    let categorias =[];
+    for (categoria of array){
+        categorias.push(categoria.category);
+    }
+    const dupCats = categorias.filter((cat, indice) => {
+        return categorias.indexOf(cat) !== indice;
+    });
+    let fragmento2 = new DocumentFragment();
+    for (let elemento of dupCats){
+        let div = document.createElement('div');
+        div.innerHTML += `<div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" value="option">
+                            <label class="form-check-label" for="checkbox">${elemento} </label>
                         </div>`
-    fragmento.appendChild(div);
-};
+                        
+        fragmento2.appendChild(div);
+    };
+    busqueda.appendChild(fragmento2);
+}
+crearChecks(eventsData.events);
 
-cuerpo.appendChild(fragmento);
+
+// SE ESCUCHAN LAS CHECKBOXES, SE GENERA ARRAY DE LABELS
+let arrayTrimeado = [];
+let checkboxes = document.querySelectorAll('input[type=checkbox]')
+checkboxes.forEach( checkbox  => checkbox.addEventListener('change',() => { 
+    let chequeados = [...checkboxes].filter(checkbox => checkbox.checked).map(elemento => elemento.nextElementSibling.innerHTML)
+     
+    arrayTrimeado = chequeados.map(element => {
+        return element.trim()});
+    console.log(arrayTrimeado);
+    filtroCruzado(fichas);
+}))
+
+function filtrarCategorias(arrayCategorias, arrayObjetos) {
+    return arrayCategorias.length === 0 ? arrayObjetos : arrayObjetos.filter(elemento => arrayCategorias.includes(elemento.category));
+}
+
+// FILTRO SEARCH
+let inputValue = ''
+let filtroBusqueda = document.getElementById('filtroBusqueda');
+let arrayFiltrado2 = [];
+
+const input = filtroBusqueda.addEventListener('keyup', (e) => {
+    inputValue = e.target.value.toLowerCase();
+        filtroCruzado(fichas);
+});
+
+function busquedaPorInput(valor, arrayObjetos) {
+    if (valor == '') return arrayObjetos;
+    let arrayNuevo = arrayObjetos.filter(el => el.name.toLowerCase().includes(valor.toLowerCase().trim()))
+    console.log(arrayNuevo);
+    return arrayNuevo;
+}
+
+// ❌❌❌ FILTRO CRUZADO ❌❌❌
+
+function filtroCruzado(array){
+    let tarjetasFiltradas = filtrarCategorias(arrayTrimeado, array);
+    let checkFiltrados = busquedaPorInput(inputValue, tarjetasFiltradas);
+    tarjetas(checkFiltrados, 'cuerpo');
+}
