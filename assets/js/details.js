fe7a5ca2ec838ee7a56a4a87f2233ae8d9f7040d
Augusto <augustoalerino@hotmail.com>
2023-03-09 17:24:49 -0300

    Datos agregados en details

--- assets/js/details.js
+++ assets/js/details.js
@@ -14,13 +14,15 @@ function mostrarTarjeta(idContainer){
                                     </div>
                                     <div class="col-md-8 border border-dark">
                                         <div class="card-body">
-                                        <h5 class="card-title">${evento.name}</h5>
-                                        <p class="card-text" style="text-align: center"><big>${evento.description}</big></p>
-                                        <p class="card-text"><small class="text-muted">Date: ${evento.date}</small></p>
-                                        <p class="card-text"><small class="text-muted">Category: ${evento.category}</small></p>
-                                        <p class="card-text"><small class="text-muted">Place: ${evento.place}</small></p>
-                                        <p class="card-text"><small class="text-muted">${evento.assistance > 0 ? "Attendance: " + evento.assistance + " people." : ""}</small></p>
-                                        <p class="card-text"><small class="text-muted">Price: $${evento.price}</small></p>
+                                        <h5 class="card-title mt-2">${evento.name}</h5>
+                                        <p class="card-text mt-1" style="text-align: center"><big>${evento.description}</big></p>
+                                        <p class="card-text mb-0"><small class="text-muted">Date: ${evento.date}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">Category: ${evento.category}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">Place: ${evento.place}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">${evento.assistance > 0 ? "Attendance: " + evento.assistance + " people." : ""}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">${evento.estimate > 0 ? "Estimate: " + evento.estimate + " people." : ""}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">${evento.capacity > 0 ? "Capacity: " + evento.capacity + " people." : ""}</small></p>
+                                        <p class="card-text mb-0"><small class="text-muted">Price: $${evento.price}</small></p>
                                         </div>
                                         <div class="btn-holder">
                                             <input type="button"  class="btn btn-danger me-5 position-relative " value="Back" onclick="history.back()" style="float:right; bottom: 0">
